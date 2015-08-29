d4e81b8c7c232cb4ff5ac4322ae9958db6e7c610
winksGeek <devonwinkler@Devons-iMac.local>
2015-08-28 23:22:15 -0600

    added comments and removed commented code

--- LightSwitch/app/src/main/java/winkler/devon/lightswitch/RootActivity.java
+++ LightSwitch/app/src/main/java/winkler/devon/lightswitch/RootActivity.java
@@ -17,37 +17,34 @@ public class RootActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //root layout
         final LinearLayout layout = new LinearLayout(this);
         layout.setOrientation(LinearLayout.VERTICAL);
         layout.setBackgroundColor(Color.DKGRAY);
 
+        //add light bulb off image
         final ImageView light = new ImageView(this);
         light.setImageResource(R.drawable.off);
-//        light.setBackgroundColor(Color.BLACK);
         layout.addView(light, new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0, 1));
 
-
+        //add switch object
         final Switch lightSwitch = new Switch(this);
-//        lightSwitch.setBackgroundColor(Color.BLACK);
         LinearLayout.LayoutParams switchLayoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, 0);
         switchLayoutParams.gravity = Gravity.CENTER_HORIZONTAL;
         switchLayoutParams.topMargin = 150;
         switchLayoutParams.bottomMargin = 150;
-//        lightSwitch.setPadding(100, 100, 100, 100);
         layout.addView(lightSwitch, switchLayoutParams);
 
+        //set switch to turn the light on and off
         lightSwitch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (lightSwitch.isChecked()) {
                     light.setImageResource(R.drawable.on);
-//                    light.setBackgroundColor(Color.WHITE);
-//                    lightSwitch.setBackgroundColor(Color.WHITE);
                     layout.setBackgroundColor(Color.WHITE);
                 } else {
                     light.setImageResource(R.drawable.off);
-//                    light.setBackgroundColor(Color.BLACK);
-//                    lightSwitch.setBackgroundColor(Color.BLACK);
                     layout.setBackgroundColor(Color.DKGRAY);
                 }
             }
